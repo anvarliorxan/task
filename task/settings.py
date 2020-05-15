cd6f0aa06f5a47fd6077b47680f12ad14fbe84a4
anvarliorxan <57799824+anvarliorxan@users.noreply.github.com>
2020-05-15 13:46:10 +0400

    Update settings.py

--- task/settings.py
+++ task/settings.py
@@ -134,7 +134,7 @@ STATIC_ROOT = os.path.join(BASE_DIR,'allstatic')
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'testtask12345@gmail.com'
-EMAIL_HOST_PASSWORD = 'service12345'
+EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
 
